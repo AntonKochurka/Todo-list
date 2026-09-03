96e376004c4582552132f917107dcc0556d12b63
AntonKochurka <antonkotchurka@gmail.com>
2026-09-03 15:20:33 +0300

    Initial commit

---
+++ frontend/src/app.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <main className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
+            <h1 className="text-4xl font-bold">
+                Todo App
+            </h1>
+        </main>
+    );
+}
+
+export default App;
